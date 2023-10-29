4857abcc999a2c7314576cb6a109526ca90be542
Lautaro Barrera <lautarobarrera12@gmail.com>
2023-10-28 22:43:46 -0300

    menú creado y opción 1 programada

--- programaContrerasContrerasBarrera.php
+++ programaContrerasContrerasBarrera.php
@@ -8,7 +8,7 @@ include_once("wordix.php");
 /**************************************/
 
 /* Apellido, Nombre. Legajo. Carrera. mail. Usuario Github */
-/* ****COMPLETAR***** */
+/* Barrera Lautaro FAI-4801 TUDW lautarobarrera12@gmail.com lautarobarrera10 */
 
 
 /**************************************/
@@ -19,19 +19,36 @@ include_once("wordix.php");
  * Obtiene una colección de palabras
  * @return array
  */
-function cargarColeccionPalabras()
-{
+function cargarColeccionPalabras(){
+    // array $coleccionPalabras
     $coleccionPalabras = [
         "MUJER", "QUESO", "FUEGO", "CASAS", "RASGO",
         "GATOS", "GOTAS", "HUEVO", "TINTO", "NAVES",
-        "VERDE", "MELON", "YUYOS", "PIANO", "PISOS"
-        /* Agregar 5 palabras más */
+        "VERDE", "MELON", "YUYOS", "PIANO", "PISOS",
+        "PERRO", "PADRE", "MADRE", "PRIMO", "CALOR"
     ];
 
     return ($coleccionPalabras);
 }
 
-/* ****COMPLETAR***** */
+/**
+ * Muestra el menú de opciones
+ * @return int opción elegida
+ */
+function seleccionarOpcion(){
+    // int $numero
+    escribirNormal("SELECIONE UNA OPCIÓN: \n");
+    escribirNormal("1) Jugar al wordix con una palabra elegida\n");
+    escribirNormal("2) Jugar al wordix con una palabra aleatoria\n");
+    escribirNormal("3) Mostrar una partida\n");
+    escribirNormal("4) Mostrar la primer partida ganadora\n");
+    escribirNormal("5) Mostrar resumen de Jugador\n");
+    escribirNormal("6) Mostrar listado de partidas ordenadas por jugador y por palabra\n");
+    escribirNormal("7) Agregar una palabra de 5 letras a Wordix\n");
+    escribirNormal("8) Salir\n");
+    $numero = solicitarNumeroEntre(1,8);
+    return $numero;
+}
 
 
 
@@ -40,39 +57,30 @@ function cargarColeccionPalabras()
 /**************************************/
 
 //Declaración de variables:
-
+$palabras; // array
+$partidas; // array
+$partida; // array
+$opcion; // int
 
 //Inicialización de variables:
-
+$palabras = cargarColeccionPalabras();
+$partidas = [];
 
 //Proceso:
 
-$partida = jugarWordix("MELON", strtolower("MaJo"));
 //print_r($partida);
-//imprimirResultado($partida);
+// imprimirResultado($partida);
 
-
-
-/*
-do {
-    $opcion = ...;
-
-    
-    switch ($opcion) {
-        case 1: 
-            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 1
-
-            break;
-        case 2: 
-            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 2
-
-            break;
-        case 3: 
-            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 3
-
-            break;
-        
-            //...
-    }
-} while ($opcion != X);
-*/
+$opcion = seleccionarOpcion();
+switch ($opcion) {
+    case 1:
+        // string $nombreUsuario $palabra, int $indicePalabra, array $partida
+        escribirNormal("Ingresa tu nombre: ");
+        $nombreUsuario = trim(fgets(STDIN));
+        escribirNormal("Ingresa el número de la palabra con la que quieres jugar: ");
+        $indicePalabra = solicitarNumeroEntre(1, 10) - 1;
+        $palabra = $palabras[$indicePalabra];
+        $partida = jugarWordix($palabra, $nombreUsuario);
+        array_push($partidas, $partida);
+        break;
+}
