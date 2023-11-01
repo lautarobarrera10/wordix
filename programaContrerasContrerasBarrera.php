034b8d660195a822e4a1ab681406c9463814bc26
lautarobarrera10 <lautarobarrera12@gmail.com>
2023-11-01 18:56:46 -0300

    arreglo unas identaciones

--- programaContrerasContrerasBarrera.php
+++ programaContrerasContrerasBarrera.php
@@ -4,7 +4,7 @@ require "menu/selectorOpciones.php";
 require "funciones/resumenJugador.php";
 require "funciones/primeraPartidaGanadora.php";
 require "funciones/ordenarColeccionPartidas.php";
-require "menu/agregarPalabra.php"; 
+require "menu/agregarPalabra.php";
 require "funciones/nombreMinuscula.php";
 /**************************************/
 /***** DATOS DE LOS INTEGRANTES *******/
@@ -56,7 +56,7 @@ do {
     switch ($opcion) {
         case 1:
             // string $nombreUsuario $palabra, int $indicePalabra, array $partida
-            $nombreUsuario=solicitarJugador();
+            $nombreUsuario = solicitarJugador();
             escribirNormal("Ingresa el número de la palabra con la que quieres jugar: ");
             $indicePalabra = solicitarNumeroEntre(1, count($palabras)) - 1;
             $palabra = $palabras[$indicePalabra];
@@ -66,7 +66,7 @@ do {
         case 2:
             // string $nombreUsuario $palabra, int $indicePalabra, array $partida
             // 
-            $nombreUsuario=solicitarJugador();
+            $nombreUsuario = solicitarJugador();
             $indicePalabra = rand(1, count($palabras) - 1);
             $palabra = $palabras[$indicePalabra];
             $partida = jugarWordix($palabra, $nombreUsuario);
@@ -113,9 +113,9 @@ do {
             break;
         case 4:
             //muestra la primera partida ganadora
-            if(!empty($partidas)){
+            if (!empty($partidas)) {
                 $partidaGanada = primeraPartidaGanada($partidas);
-                if(!empty($partidaGanada)){
+                if (!empty($partidaGanada)) {
                     $palabraWordix = $partidaGanada["palabraWordix"];
                     $jugador = $partidaGanada["jugador"];
                     $intentos = $partidaGanada["intentos"];
@@ -124,29 +124,29 @@ do {
                     escribirNormal("Partida WORDIX $numeroPartida: palabra $palabraWordix\n");
                     escribirNormal("Jugador: $jugador\n");
                     escribirNormal("Puntaje: $puntaje puntos\n");
-                }else{
+                } else {
                     escribirNormal("no han ganado ninguna partida");
                 }
-            }else {
+            } else {
                 escribirNormal("\nAún no se han jugado partidas.\nJuega una partida para poder ver la primera partida ganada.\n\n");
             }
             break;
         case 5:
             escribirNormal("ingrese el nombre del jugador :");
             $nombreJugador = trim(fgets(STDIN));
-            $resumen = resumenJugador($partidas,$nombreJugador);
-            foreach($resumen as $indice => $elemento){
-                if(!is_array($elemento)){ 
-                    escribirNormal($indice. " : " . $elemento. "\n");
-                }else{
-                    foreach($elemento as $ind => $elem){
-                        escribirNormal(" intento $ind : $elem \n" );
+            $resumen = resumenJugador($partidas, $nombreJugador);
+            foreach ($resumen as $indice => $elemento) {
+                if (!is_array($elemento)) {
+                    escribirNormal($indice . " : " . $elemento . "\n");
+                } else {
+                    foreach ($elemento as $ind => $elem) {
+                        escribirNormal(" intento $ind : $elem \n");
                     }
                 }
             }
-            break;   
-            case 7:
-                $palabras = agregarPalabra($palabras);
-                // print_r($palabras);
+            break;
+        case 7:
+            $palabras = agregarPalabra($palabras);
+            break;
     }
 } while ($opcion != 8);
